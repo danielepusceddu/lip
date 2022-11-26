26a6240cbadfc410b2ef56c052de48af6a45b49e
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2022-11-27 00:46:26 +0100

    Implement trace

--- imp/while/lib/main.ml
+++ imp/while/lib/main.ml
@@ -4,6 +4,17 @@ type exprval = Bool of bool | Nat of int        (* value of an expression *)
 type state = ide -> exprval                     (* state = map from identifiers to expression values *)
 type conf = St of state | Cmd of cmd * state    (* configuration = state | (command,state) *)
 
+exception TypeException
+let bool_of_exprval = function
+  | Bool(b) -> b
+  | _ -> raise TypeException
+;;
+
+let nat_of_exprval = function
+  | Nat(i) -> i
+  | _ -> raise TypeException
+;;
+
 let parse (s : string) : cmd =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
@@ -13,6 +24,7 @@ let string_of_val = function
   | Nat(i) -> string_of_int i
   | Bool(b) -> string_of_bool b
 ;;
+
 let rec string_of_expr = function
   | True -> "true"
   | False -> "false"
@@ -34,7 +46,6 @@ let rec string_of_cmd = function
   | If(e,c1,c2) -> "if " ^ (string_of_expr e) ^ " then " ^ (string_of_cmd c1) ^ " else " ^ (string_of_cmd c2)
   | While(e,c) -> "while " ^ (string_of_expr e) ^ " do " ^ (string_of_cmd c)
 
-let eval_expr _ _ = failwith "Not implemented"
 let string_of_state s v = 
   "{"^
   (List.fold_left (fun acc x -> acc ^"("^x^":="^(string_of_val (s x))^"), ") "" v)
@@ -83,11 +94,60 @@ let eval_expr state e =
         )
   in help e
 ;;
+exception UnboundVar of string
+exception NoRuleApplies
 
-let trace1 _ = failwith "Not implemented"
+let rec trace1 = function
+  | St(_) -> raise NoRuleApplies
+  | Cmd(c,s) -> match c with
+    | Skip -> St(s)
 
-let trace _ _ = failwith "Not implemented"
+    | Assign(x,e) -> 
+        let e' = eval_expr s e in
+        St(fun y -> if y=x then e' else s y)
 
-let last _ = failwith "Not implemented"
+    | If(e,c1,c2) -> 
+        let e' = eval_expr s e in (match e' with
+          | Bool(true) -> Cmd(c1,s)
+          | Bool(false) -> Cmd(c2,s)
+          | _ -> raise TypeException)
 
-let vars_of_cmd _ = failwith "Not implemented"
+    | While(e,c) -> 
+        let e' = eval_expr s e in (match e' with
+          | Bool(true) -> Cmd(Seq(c,While(e,c)),s)
+          | Bool(false) -> St(s)
+          | _ -> raise TypeException)
+
+    | Seq(c1,c2) -> 
+        let conf' = trace1 (Cmd(c1,s)) in (match conf' with
+          | St(s') -> Cmd(c2,s')
+          | Cmd(c1',s') -> Cmd(Seq(c1',c2),s'))
+;;
+
+
+let trace n cmd = 
+  let rec trace' n con = 
+    if n=0 then [con]
+    else try (let con' = trace1 con in
+      con'::(trace' (n-1) con'))
+    with _ -> [con]
+  in let init = Cmd(cmd,(fun x -> raise (UnboundVar (x))))
+  in init::(trace' n init)
+;;
+
+let rec last = function
+  | [] -> failwith "Empty list" 
+  | h::[] -> h
+  | _::t -> last t
+;;
+
+let vars_of_cmd c = 
+  let has l x = List.exists (fun y -> x=y) l in
+  let rec help seen = function
+    | Skip -> []
+    | Assign(x,_) -> if has seen x then seen else seen@[x]
+    | Seq(c1,c2) -> (help seen c1)@(help seen c2)
+    | If(_,c1,c2) -> (help seen c1)@(help seen c2)
+    | While(_,c) -> (help seen c)
+  in help [] c
+;;
